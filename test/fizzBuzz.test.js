4502f588a2bebbde3b41fd50f40d22f046e75b1d
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-02-19 13:12:30 +0100

    Change FizzBuzz test to manipulate object and properties

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -6,11 +6,29 @@ describe('FizzBuzz', function() {
 
   it('Should transform array with FizzBuzz', function() {
     const input = [1, 2, 3, 4, 5, 6, 7, 8, 9,
-      10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-    const output = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz',
-      'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz']
-
+      10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] 
+    const output = [
+      { value: 1, messages: []},
+      { value: 2, messages: []},
+      { value: 3, messages: ['Fizz']},
+      { value: 4, messages: []},
+      { value: 5, messages: ['Buzz']},
+      { value: 6, messages: ['Fizz']},
+      { value: 7, messages: []},
+      { value: 8, messages: []},
+      { value: 9, messages: ['Fizz']},
+      { value: 10, messages: ['Buzz']},
+      { value: 11, messages: []},
+      { value: 12, messages: ['Fizz']},
+      { value: 13, messages: []},
+      { value: 14, messages: []},
+      { value: 15, messages: ['Fizz', 'Buzz']},
+      { value: 16, messages: []},
+      { value: 17, messages: []},
+      { value: 18, messages: ['Fizz']},
+      { value: 19, messages: []},
+      { value: 20, messages: ['Buzz']}
+    ]
     expect(fizzBuzz(input)).to.be.eql(output);
   });
 
