ff2435654cd1b287eecfcd17bc679fc2634fb7dc
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-02-19 15:30:12 +0100

    Solution to pass test to concatObject

--- concatObject.js
+++ concatObject.js
@@ -1,6 +1,9 @@
 
 const concatObject = function (input1, input2) {
-  return "tutu"
+  return {
+    names: [input1.name, input2.name],
+    money: input1.money + input2.money
+  }
 }
 
 module.exports = concatObject
