5407d58bce2a6a747667e34658455dded91f9c96
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-02-26 16:11:57 +0100

    Solution to exercise average

--- average.js
+++ average.js
@@ -1,6 +1,10 @@
 
-const average = function () {
-
+const average = function (input) {
+  var sum = 0
+  for (let i = 0; i < input.length; i++) {
+    sum += input[i]
+  }
+  return sum / input.length
 }
 
 module.exports = average
