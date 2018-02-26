d8cb8b60c870133c7025a7e0b7515f8c1812a4a4
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-02-26 16:16:21 +0100

    Solution exercise getUsers

--- getUsers.js
+++ getUsers.js
@@ -1,6 +1,30 @@
-
-const getUsers = function () {
-
+const USERS = [
+  { name: "John", money: 300 },
+  { name: "Jane", money: 500 },
+  { name: "Tom", money: 200 },
+  { name: "Lucy", money: 800 },
+  { name: "Hellboy", money: 666 },
+  { name: "Bruce", money: 10 },
+  { name: "Emma", money: 600 },
+  { name: "Jack", money: 500 },
+  { name: "Bob", money: 450 }
+]
+const getUsers = function (predicate) {
+  if (!predicate) {
+    return USERS
+  } else {
+    const output = []
+    for (let i = 0; i < USERS.length; i++) {
+      var user = USERS[i]
+      if (predicate(user)) {
+        output.push({
+          name: user.name,
+          money: user.money
+        })
+      }
+    }
+    return output
+  }
 }
 
 module.exports = getUsers
